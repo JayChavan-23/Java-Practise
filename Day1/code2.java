c7a6f108c442c7602ae89e9399fe6e5adbbe3e45
JayChavan-23 <jaychavan2223@gmail.com>
2022-03-11 02:26:57 +0530

    code3

--- Day1/code2.java
+++ Day1/code2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class code2 {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number");
+        System.out.print("Enter a number : ");
         int num = sc.nextInt();
         System.out.println("Your number was "+num);
     }
---
+++ Day1/code3.java
@@ -0,0 +1,9 @@
+package Day1;
+//Program to Add Two Integers
+public class code3 {
+    public static void main(String[] args){
+    int f = 10 ;
+    int s  = 30;
+        System.out.println(f+s);
+    }
+}
