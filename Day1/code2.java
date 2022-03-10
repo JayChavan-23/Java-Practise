aff952949bd96e0550c4544e62f4661463a91b5a
JayChavan-23 <jaychavan2223@gmail.com>
2022-03-11 02:22:50 +0530

    code2

--- Day1/code2.java
+++ Day1/code2.java
@@ -1,5 +1,13 @@
 package Day1;
 
+import java.util.Scanner;
+
+//How to Print an Integer entered by an user
 public class code2 {
-    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        System.out.println("Your number was "+num);
+    }
 }
